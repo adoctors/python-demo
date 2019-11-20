ad4000530de3d93441ce978483b2fb6c0773dc3d
qinkeliang <qinkeliang@163.com>
2019-11-20 18:45:49 +0800

    base/01/01.py

---
+++ base/01/01.py
@@ -0,0 +1,56 @@
+# import sys
+
+# print(sys.version)
+# print(sys.version_info)
+# print(sys.api_version)
+
+
+# import turtle
+    
+# turtle.pensize(4)
+# turtle.pencolor('green')
+
+# turtle.forward(100)
+# turtle.right(90)
+# turtle.forward(100)
+# turtle.right(90)
+# turtle.forward(100)
+# turtle.right(90)
+# turtle.forward(100)
+
+# turtle.mainloop()
+
+# a = int(input('a = '))
+# b = int(input('b = '))
+# print('%d + %d = %d' % (a, b, a + b))
+# print('%d - %d = %d' % (a, b, a - b))
+# print('%d * %d = %d' % (a, b, a * b))
+# print('%d / %d = %f' % (a, b, a / b))
+# print('%d // %d = %d' % (a, b, a // b))
+# print('%d %% %d = %d' % (a, b, a % b))
+# print('%d ** %d = %d' % (a, b, a ** b))
+
+# username = input('username:')
+# password = input('password:')
+
+# if username == 'admin' and password == '123':
+# 	print('success!')
+# else:
+# 	print('fail')
+
+# score = float(input('请输入成绩：'))
+# if score >= 90:
+# 	grade = 'A'
+# elif score >= 80:
+# 	grade = 'B'
+# elif score >= 70:
+# 	grade = 'C'
+# else:
+# 	grade = 'D'
+
+# print('对应的等级是：',grade)
+
+# sum = 0
+# for x in range(101):
+# 	sum +=x
+# print(sum)
